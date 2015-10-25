0098fc48f902ae8ac951870156b5ab47e9836d25
Christopher Stott <christopherstott@gmail.com>
2015-10-24 17:58:29 -0700

    Initial Commit

---
+++ BBTestAppXcode71/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTestAppXcode71
+//
+//  Created by Chris on 2015-10-24.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBTestAppXcode71/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBTestAppXcode71
+//
+//  Created by Chris on 2015-10-24.
+//  Copyright © 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
